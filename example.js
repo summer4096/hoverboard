6e6c4a30e0b8737d5b2e4d0c15d1192ca63602bf
Summer Davies <summer4096@users.noreply.github.com>
2015-02-03 18:49:02 -0500

    properly wrap earth

--- example.js
+++ example.js
@@ -6,8 +6,8 @@ var map = L.map('map', {
 
 //L.tileLayer('http://{s}.tile.stamen.com/terrain-background/{z}/{x}/{y}.jpg').addTo(map);
 
-var url = 'http://{s}.tile.openstreetmap.us/vectiles-highroad/{z}/{x}/{y}.topojson';
-//var url = 'https://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v5,mapbox.mapbox-terrain-v2/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoiZmFyYWRheTIiLCJhIjoiTUVHbDl5OCJ9.buFaqIdaIM3iXr1BOYKpsQ';
+//var url = 'http://{s}.tile.openstreetmap.us/vectiles-highroad/{z}/{x}/{y}.topojson';
+var url = 'https://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v5,mapbox.mapbox-terrain-v2/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoiZmFyYWRheTIiLCJhIjoiTUVHbDl5OCJ9.buFaqIdaIM3iXr1BOYKpsQ';
 
 var colors = {
   land: '#FCFBE7',
@@ -31,70 +31,9 @@ var colors = {
 };
 
 L.tileLayer.hoverboard(url, {hidpiPolyfill: true})
-/*
-  .render('landuse')
-    .minZoom(12)
-    .fillBy('class', {
-      cemetery: colors.cemetery,
-      college: colors.school,
-      commercial: colors.industrial,
-      common: colors.park,
-      forest: colors.wooded,
-      golf_course: colors.sports,
-      grass: colors.grass,
-      hospital: colors.hospital,
-      industrial: colors.industrial,
-      park: colors.park,
-      parking: colors.parking,
-      pedestrian: colors.pedestrian_fill,
-      pitch: colors.sports,
-      residential: colors.residential,
-      school: colors.school,
-      sports_center: colors.sports,
-      stadium: colors.sports,
-      university: colors.school,
-      wood: colors.wooded
-    })
-
-  .render('hillshade')
-    .fillBy('level', {
-      // Hillshade is now defined by numeric shade level in v2
-      94: '#f2f3f3',
-      90: '#cdcdd1',
-      89: '#a8a8b1',
-      78: '#868592',
-      67: '#646373',
-      56: '#444456'
-    })
-
-  .render('contour')
-    .stroke(0.6, 'rgba(20,20,35,0.3')
-    // Try out hypsometric nonsense here:
-    /*.fillBy('ele', {
-      10: '#000',
-      20: '#111'
-      etc . . .
-    })
-
-  .render('road')
-    .where('type', ['motorway', 'trunk'])
-    .stroke(1.75, 'rgba(2555, 255, 255, 0.5)')
-    .stroke(0.75, colors.big_road)
-
-  .render('road')
-    .whereNot('type', ['motorway', 'trunk'])
-    .stroke(1, 'rgba(255, 255, 255, 0.5)')
-    .stroke(0.5, colors.little_road)
-
-  .render('building')
-    .fill('#888896')
-    .stroke(0.5, 'rgba(0,0,0,0.4)')
 
   .render('water')
     .fill(colors.water)
-
-  .render('waterway')
-    .stroke(1, colors.water)*/
 
   .addTo(map);
 
